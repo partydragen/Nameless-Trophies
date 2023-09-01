042cd99b7c844c29516221709654d93d1685ea25
Partydragen <admin@partydragen.com>
2023-09-02 00:15:44 +0200

    Add linked certain integration trophy

---
+++ upload/modules/Trophies/classes/Listeners/UserLinkedIntegrationListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Trophies\Listeners;
+
+use Trophies;
+use UserIntegrationVerifiedEvent;
+use UserTrophies;
+use User;
+use DB;
+
+class UserLinkedIntegrationListener {
+    public static function execute(UserIntegrationVerifiedEvent $event): void {
+        $trophies = Trophies::getInstance()->getTrophies();
+        $user_trophies = new UserTrophies($event->user);
+
+        if (array_key_exists('linkedIntegration', $trophies)) {
+            foreach ($trophies['linkedIntegration'] as $trophy) {
+                $data = json_decode($trophy->data()->data, true) ?? [];
+
+                if (isset($data['integration']) && $data['integration'] == $event->integration->getName()) {
+                    if (!$user_trophies->hasTrophy($trophy)) {
+                        $user_trophies->rewardTrophy($trophy);
+                    }
+                }
+            }
+        }
+    }
+}
