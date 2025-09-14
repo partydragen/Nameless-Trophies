14c194c41c8f499ca0dac309126cdffe480dfb5a
partydragen <admin@partydragen.com>
2025-09-14 20:10:18 +0200

    Release v1.2.0

--- upload/modules/Trophies/module.php
+++ upload/modules/Trophies/module.php
@@ -20,8 +20,8 @@ class Trophies_Module extends Module {
 
         $name = 'Trophies';
         $author = '<a href="https://partydragen.com" target="_blank" rel="nofollow noopener">Partydragen</a> and my <a href="https://partydragen.com/supporters/" target="_blank">Sponsors</a>';
-        $module_version = '1.1.4';
-        $nameless_version = '2.2.0';
+        $module_version = '1.2.0';
+        $nameless_version = '2.2.4';
 
         parent::__construct($this, $name, $author, $module_version, $nameless_version);
 
